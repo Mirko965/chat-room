0d369b5b2b3a02a380713a66ee936364f4eef90d
Mirko965 <mirkojelic.jelic@gmail.com>
2018-03-05 01:51:47 +0100

    add template

--- public/js/index.js
+++ public/js/index.js
@@ -8,22 +8,33 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function (msg) {
-  console.log('message:',msg)
-  let list = document.createElement('LI');
-  let text = document.createTextNode(`${msg.from}: ${msg.text}`);
-  list.appendChild(text)
-  document.getElementById('messages').appendChild(list)
+  let momentTime = moment().format('h:mm a')
+  let source = document.getElementById('message-template').innerHTML
+  let template = Handlebars.compile(source)
+
+  let context = {
+    text:msg.text,
+    from:msg.from,
+    create:momentTime
+  }
+  let html = template(context)
+  document.querySelector('#messages').insertAdjacentHTML('beforeend',html)
+
 })
 
 socket.on('newLocationMessage',function (message) {
-  let li = document.createElement('LI');
-  let a = document.createElement('A');
-  li.textContent = `${message.from}: `;
-  a.href = message.url;
-  a.target = '_blank'
-  a.text = 'My Location'
-  li.appendChild(a)
-  document.getElementById('messages').appendChild(li)
+
+  let momentTime = moment().format('h:mm a')
+  let source = document.getElementById('newLocationMessage-template').innerHTML
+  let template = Handlebars.compile(source)
+
+  let context = {
+    from: message.from,
+    url:message.url,
+    create: momentTime
+  }
+  let html = template(context)
+  document.querySelector('#messages').insertAdjacentHTML('beforeend',html)
 
 })
 
@@ -34,32 +45,39 @@ let myNode = document.getElementById('messageForm');
 
 myNode.addEventListener('submit',async function (event) {
   event.preventDefault();
-  const text = document.messageForm.message.value
+  let text = document.messageForm.message.value
 
-  let emit =  socket.emit('createMessage',{
+  let emit = socket.emit('createMessage',{
     from: 'User',
     text
   },function () {
-
+    text = ''
   })
-  let reset = myNode.reset()
   await emit
-  await reset
+  await myNode.reset()
 })
 
 let locationButton = document.getElementById('sendLocation');
+
 locationButton.addEventListener('click', function () {
   if (!navigator.geolocation){
     return alert('Geolocation not supported on your browser')
   }
+  locationButton.disabled = 'disabled';
+  locationButton.textContent = 'Sending....'
 
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.disabled = ''
+    locationButton.textContent = 'Send Location'
+
     socket.emit('createLocationMessage',{
       latitude:position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function () {
     alert('Unable fetch location')
+    locationButton.disabled = ''
+    locationButton.textContent = 'Send Location'
   })
 })
 
